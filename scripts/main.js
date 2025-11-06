5a454aabee4980dacff7fe931dc2e36dd665d8d3
maxwellbytes <brownmh3@appstate.edu>
2025-11-06 01:19:29 -0500

    decent progress if i do say so myself
    finished skills section, added taskbar, updated a lot of stuff, working on getting minimize button to work

--- scripts/main.js
+++ scripts/main.js
@@ -1,124 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const nameContainer = document.getElementById("nameHeader");
-    const name = "Maxwell Brown | CS Senior at Appalachian State University"
-
-    
-    const sweepLink = document.querySelector('#linkedinLine');
-    const sweepLine = sweepLink.querySelector('span');
-
-    sweepLink.addEventListener('mouseenter', () => {
-    anime({
-        targets: sweepLine,
-        width: ['0%', '100%'],
-        easing: 'easeOutExpo',
-        duration: 400
-    });
-    });
-
-    sweepLink.addEventListener('mouseleave', () => {
-    anime({
-        targets: sweepLine,
-        width: ['100%', '0%'],
-        easing: 'easeInExpo',
-        duration: 400
-    });
-    });
-
-    
-
+document.addEventListener('DOMContentLoaded', function() {
+    const nameContainer = document.getElementById('nameHeader');
+    const name = 'Maxwell Brown | CS Senior at Appalachian State University';
 
     anime({
         targets: nameContainer,
-        textContent: ["", name],
-        easing: "easeInOutQuad",
+        textContent: ['', name],
         duration: 2000,
+        easing: 'easeInOutQuad',
         delay: 500,
         update: function(anim) {
             nameContainer.textContent = name.substring(0, Math.round(anim.progress / 100 * name.length));
         }
     });
-})
+});
+
+// const taskbarWindow = document.getElementById('taskbarWindows')
+// const portfolioWindow = document.getElementById('window');
+// let portfolioButton = document.createElement('button');
+// portfolioButton.textContent = 'Portfolio.exe';
+// portfolioButton.className = 'bg-b300 hover:bg-b200 text-white text-sm px-3 py-1 rounded transition-all duration-200';
+
 function animateTabContent() {
     anime({
-        targets: '.tab-content',
+        targets: '._tabContent',
         opacity: [0, 1],
         translateY: [20, 0],
         duration: 500,
         easing: 'easeOutQuad'
     });
 }
-
-
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     var activeTab = "welcome";
-//     //Methods to call on start
-//     //eventListeners();
-//     // initializeAnimations();
-
-//     // function initializeAnimations() {
-//     //     // Split the text into characters
-//     //     const text = document.querySelector('h2');
-//     //     const characters = text.innerHTML.split('');
-        
-//     //     // Wrap each character in a span
-//     //     text.innerHTML = characters
-//     //         .map(char => `<span class="char">${char}</span>`)
-//     //         .join('');
-
-//     //     // Create the animation
-//     //     anime({
-//     //         targets: '.char',
-//     //         //change this to make the characters move more or less
-//     //         translateY: [-50, 0],
-//     //         rotate: {
-//     //             value: ['360deg', '0deg'],
-//     //             duration: 1000,
-//     //             easing: 'easeInOutSine'
-//     //         },
-//     //         opacity: [0, 1],
-//     //         //change this to get different animation styles
-//     //         easing: 'easeOutExpo',
-//     //         //adjust this to make it faster or slower
-//     //         duration: 2000,
-//     //         //changes how quickly characters appear after each other
-//     //         delay: anime.stagger(100),
-//     //         //set this to false if i want it to only play once
-//     //         loop: true
-//     //     });
-//     // }
-// /*
-//     function eventListeners(){
-//         document.getElementById('welcome_tab').addEventListener('click', function() {
-//             if (activeTab != "welcome") {
-//                 changeActiveTab("welcome")
-//             }
-//         })
-//         document.getElementById('about_tab').addEventListener('click', function() {
-//             if (activeTab != "about"){
-//                 changeActiveTab("about")
-//             }
-//         })
-//         document.getElementById('projects_tab').addEventListener('click', function() {
-//             if (activeTab != "projects"){
-//                 changeActiveTab("projects")
-//             }
-//         })
-//         document.getElementById('resume_tab').addEventListener('click', function() {
-//             if (activeTab != "resume"){
-//                 changeActiveTab("resume")
-//             }
-//         })
-//     }
-
-//     function changeActiveTab(newActive){
-//         let newTab = document.getElementById(`${newActive}_tab`)
-//         newTab.classList.add('tab-active')
-
-//         let currTab = document.getElementById(`${activeTab}_tab`)
-//         currTab.classList.remove('tab-active')
-
-//         activeTab = newActive
-//     }
-//         */
-// });
